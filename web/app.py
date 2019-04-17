1519d46ba05e30bf59bf75047b451af3bcac4fac
kaoss <swojak.a@gmail.com>
2019-04-17 19:29:47 +0200

    add simple post request

--- web/app.py
+++ web/app.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 
 from flask import Flask
-from flask import jsonify, request
+from flask import jsonify, request, abort
 
 app = Flask(__name__)
 
 from models import test_result
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        if not request.json:
+            abort(400)
+        if "name" not in request.json:
+            abort(401)
+        new_test_case = {"name" : request.json["name"]}
+        test_result.append(new_test_case)
+        return jsonify({"data" : test_result}), 201
 
     test = test_result
 
