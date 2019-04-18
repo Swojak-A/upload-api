212f8d6e3bb37762040901c7fb5bdc9eba46968b
kaoss <swojak.a@gmail.com>
2019-04-18 22:46:03 +0200

    format(web): turn off SQLAlchemy track modification

--- web/config.py
+++ web/config.py
@@ -1,13 +1,13 @@
 from credentials import db_key
 
 class BaseConfig(object):
-	SECRET_KEY = 'hi'
+    SECRET_KEY = 'hi'
 
-	#DB
-	SQLALCHEMY_DATABASE_URI = db_key
+    #DB
+    SQLALCHEMY_DATABASE_URI = db_key
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-
-	# FILE SETTINGS
-	UPLOAD_FOLDER = 'uploads'
-	ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-	MAX_CONTENT_LENGTH = 10 * 1024 * 1024
+    # FILE SETTINGS
+    UPLOAD_FOLDER = 'uploads'
+    ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+    MAX_CONTENT_LENGTH = 10 * 1024 * 1024
