38e0b4c0f56a5a71f07cfc7aeb26e644acd75775
kaoss <swojak.a@gmail.com>
2019-04-18 08:55:35 +0200

    test(web): add test for correct file upload

--- web/tests.py
+++ web/tests.py
@@ -12,20 +12,33 @@ class AppTestCase(unittest.TestCase):
         app.config['DEBUG'] = False
         self.app = app.test_client()
 
+    """ GET test """
+
     def test_index_get(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
-    def test_index_post(self):
-        mock_json = {"name" : "Test_name"}
-        response = self.app.post('/', json=mock_json)
+    """ POST tests """
+
+    # tests from earlier version
+    # def test_index_post(self):
+    #     mock_json = {"name" : "Test_name"}
+    #     response = self.app.post('/', json=mock_json)
+    #     self.assertEqual(response.status_code, 201)
+    #
+    # def test_index_post_no_json(self):
+    #     response = self.app.post('/', data=None)
+    #     self.assertEqual(response.status_code, 400)
+
+    def test_upload(self):
+        data = {'file': (BytesIO(b'my file content'), "test_file.jpg")}
+        response = self.app.post('/', data=data,
+                                 follow_redirects=True,
+                                 content_type='multipart/form-data')
         self.assertEqual(response.status_code, 201)
 
-    def test_index_post_no_json(self):
-        response = self.app.post('/', data=None)
-        self.assertEqual(response.status_code, 400)
 
 
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
