cc2cc863c571165432192bbbef42c99f198abcbc
kaoss <swojak.a@gmail.com>
2019-04-18 19:21:54 +0200

    test(web): add test to check if file exists on s3

--- web/tests.py
+++ web/tests.py
@@ -1,8 +1,10 @@
 import unittest
 from io import BytesIO
+import boto3
 
 from app import app
 
+from credentials import aws_access_key_id, aws_secret_access_key
 
 class AppTestCase(unittest.TestCase):
 
@@ -65,8 +67,19 @@ class AppTestCase(unittest.TestCase):
                                 content_type='multipart/form-data')
         self.assertEqual(response.status_code, 413)
 
+    """ UPLOAD TESTS """
 
-
+    def test_if_file_exists_on_s3(self):
+        # data = {'file': (BytesIO(b'my file content'), 'test_file.jpg')}
+        # response = self.app.post('/', data=data,
+        #                          follow_redirects=True,
+        #                          content_type='multipart/form-data')
+        s3 = boto3.client('s3',
+                          aws_access_key_id=aws_access_key_id,
+                          aws_secret_access_key=aws_secret_access_key)
+        aws_response = s3.list_objects(Prefix='uploads/test_file.jpg', Bucket='upload-api-task')
+        result = "Contents" in aws_response
+        self.assertEqual(result, True)
 
 
 if __name__ == "__main__":
