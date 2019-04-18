51e3595d0a91f6dc08e12bd949ed3eeb28947d51
kaoss <swojak.a@gmail.com>
2019-04-19 00:45:24 +0200

    feat(web): add url to Upload model

--- web/models.py
+++ web/models.py
@@ -8,8 +8,10 @@ class Upload(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String, nullable=False)
+    url = db.Column(db.String, nullable=False)
     date_uploaded = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self, filename):
+    def __init__(self, filename, url):
         self.filename = filename
+        self.url = url
         self.date_uploaded = datetime.datetime.now()
