adb3e9c4871628733018a58ca5218a443724ce83
kaoss <swojak.a@gmail.com>
2019-04-18 21:59:20 +0200

    feat(web): fix to ORM models

--- web/models.py
+++ web/models.py
@@ -7,10 +7,9 @@ class Upload(db.Model):
     __tablename__ = 'uploads'
 
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String, nullable=False)
+    filename = db.Column(db.String, nullable=False)
     date_uploaded = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self, **kwargs):
-        super(Upload, self).__init__(**kwargs)
-        # do custom initialization here
-        self.date_uploaded = datetime.datetime.utcnow()
+    def __init__(self, filename):
+        self.filename = filename
+        self.date_uploaded = datetime.datetime.now()
