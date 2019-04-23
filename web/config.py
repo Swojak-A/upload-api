6d32ae471169b6c2751406f5ecb8e29e3837f821
kaoss <swojak.a@gmail.com>
2019-04-23 19:45:47 +0200

    refactor(web): move config to environs

--- web/config.py
+++ web/config.py
@@ -1,13 +1,16 @@
-from credentials import db_key
+import os
 
 class BaseConfig(object):
-    SECRET_KEY = 'hi'
+    SECRET_KEY = os.environ['SECRET_KEY']
+    DEBUG = True
 
-    #DB
-    SQLALCHEMY_DATABASE_URI = db_key
+    """ DB """
+    DB_NAME = os.environ['DB_KEY']
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{}'.format(DB_NAME)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # FILE SETTINGS
+    """ FILE SETTINGS """
     UPLOAD_FOLDER = 'uploads'
     ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-    MAX_CONTENT_LENGTH = 10 * 1024 * 1024
+    MAX_CONTENT_LENGTH = 5 * 1024 * 1024
+
